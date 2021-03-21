ab1805d39a481ef87b243271072c86f8a5b34158
KazikKluz <kazik.kluz@gmail.com>
2021-03-21 00:25:02 +0000

    styling gallery items animation

--- components/gallery-item/gallery-item.component.jsx
+++ components/gallery-item/gallery-item.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-import styles from "./gallery-item.module.css";
+import styles from "./gallery-item.module.scss";
 
 const GalleryItem = ({ src }) => (
   <div className={styles.gallery_item}>
     <div className={styles.image_container}>
       <img className={styles.image} alt="" src={src} />
     </div>
+    <p className={styles.description}>Some text</p>
   </div>
 );
 
