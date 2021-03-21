3673d4dadaca147d524903f6fb97283af3f821ec
KazikKluz <kazik.kluz@gmail.com>
2021-03-21 17:09:20 +0000

    something done

--- components/gallery-item/gallery-item.component.jsx
+++ components/gallery-item/gallery-item.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 import styles from "./gallery-item.module.scss";
 
 const GalleryItem = ({ src }) => (
   <div className={styles.gallery_item}>
     <div className={styles.image_container}>
-      <img className={styles.image} alt="" src={src} />
+      <Image
+        className={styles.image}
+        width="300px"
+        height="300px"
+        alt=""
+        src={src}
+      />
     </div>
     <p className={styles.description}>Some text</p>
   </div>
