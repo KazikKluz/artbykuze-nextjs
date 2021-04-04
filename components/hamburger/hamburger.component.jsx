cca1d84f0db797b201bf572c8b3d28da65f33914
KazikKluz <kazik.kluz@gmail.com>
2021-04-04 21:45:10 +0100

    hamburger now hides when on large screen

--- components/hamburger/hamburger.component.jsx
+++ components/hamburger/hamburger.component.jsx
@@ -4,20 +4,13 @@ const Hamburger = ({ active, handleClick }) => {
   return (
     <button
       onClick={() => handleClick()}
-      className={`hamburger hamburger--spring u-hide-800 
+      className={`hamburger hamburger--spring  
        ${active}`}
       type="button"
     >
       <span className="hamburger-box">
         <span className="hamburger-inner"></span>
       </span>
-      <style jsx>{`
-        @media screen and (min-width: 800px) {
-          .hambuger {
-            display: none;
-          }
-        }
-      `}</style>
     </button>
   );
 };
